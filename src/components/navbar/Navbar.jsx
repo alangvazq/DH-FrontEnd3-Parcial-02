51c54ad47a7d4b81a4f80ee9249da13a3de9f017
alangvazq <alan.g.vazq@gmail.com>
2024-12-09 20:29:10 -0600

    Estilos a navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link to="/" className={styles.navLink}>Home</Link>
         </li>
-        <li>
-          <Link to="/contact">Contact</Link>
+        <li className={styles.navItem}>
+          <Link to="/product" className={styles.navLink}>Products</Link>
         </li>
-        <li>
-          <Link to="/product">Product</Link>
+        <li className={styles.navItem}>
+          <Link to="/favorites" className={styles.navLink}>Favorites</Link>
         </li>
-        <li>
-          <Link to="/favorites">Favorites</Link> {/* Añade esta línea */}
+        <li className={styles.navItem}>
+          <Link to="/contact" className={styles.navLink}>Contact</Link>
         </li>
       </ul>
     </nav>
