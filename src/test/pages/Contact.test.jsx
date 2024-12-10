271d8584f29df97e77e42ef6b1d32d0f83aa6a0a
alangvazq <alan.g.vazq@gmail.com>
2024-12-09 19:46:32 -0600

    Ajustes varios

--- src/test/pages/Contact.test.jsx
+++ src/test/pages/Contact.test.jsx
@@ -42,18 +42,18 @@ describe("Test <Contact />", () => {
     const buttonSubmit = screen.getByRole("button", { name: /send/i });
 
     // Act
-    await user.type(inputName, "Humberto");
-    await user.type(inputLastname, "Gomez");
-    await user.type(inputEmail, "humberto@correo.com");
+    await user.type(inputName, "Alan");
+    await user.type(inputLastname, "García");
+    await user.type(inputEmail, "alan@correo.com");
     await user.type(inputPhone, "1234567890");
-    await user.type(textareaMessage, "Este es un mensaje de prueba.");
+    await user.type(textareaMessage, "Mensaje de prueba.");
     user.click(buttonSubmit);
 
     // Assert
-    expect(inputName.value).toBe("Humberto");
-    expect(inputLastname.value).toBe("Gomez");
-    expect(inputEmail.value).toBe("humberto@correo.com");
+    expect(inputName.value).toBe("Alan");
+    expect(inputLastname.value).toBe("García");
+    expect(inputEmail.value).toBe("alan@correo.com");
     expect(inputPhone.value).toBe("1234567890");
-    expect(textareaMessage.value).toBe("Este es un mensaje de prueba.");
+    expect(textareaMessage.value).toBe("Mensaje de prueba.");
   });
 });
