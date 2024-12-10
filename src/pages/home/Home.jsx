5a59f1362939535fb020bfe58954530a394fc62a
alangvazq <alan.g.vazq@gmail.com>
2024-12-09 20:33:55 -0600

    Estilos a home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
+import styles from './Home.module.css';
+
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className={styles.container}>
+      <img className={styles.banner} src="https://cdn11.bigcommerce.com/s-kzjsut/product_images/uploaded_images/amiibobanner.jpg" alt="Amiibo Banner" />
     </div>
   );
 }
