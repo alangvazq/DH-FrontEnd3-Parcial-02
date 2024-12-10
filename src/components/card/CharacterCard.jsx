9aa7df6013721b290e00ba3da9cf23f501cd3455
alangvazq <alan.g.vazq@gmail.com>
2024-12-09 21:22:06 -0600

    sweetalert agregado

--- src/components/card/CharacterCard.jsx
+++ src/components/card/CharacterCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./CharacterCard.module.css";
 import { useFavorites } from "../../context/FavoritesContext";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const CharacterCard = ({ character, image, amiiboSeries, tail }) => {
     const { addFavorite } = useFavorites();
@@ -11,13 +12,23 @@ const CharacterCard = ({ character, image, amiiboSeries, tail }) => {
         navigate(`/character/${tail}`);
     };
 
+    const handleAddFavorite = () => {
+        addFavorite({ character, image, amiiboSeries, tail });
+        Swal.fire({
+            title: 'Added to Favorites!',
+            text: `${character} has been added to your favorites.`,
+            icon: 'success',
+            confirmButtonText: 'OK'
+        });
+    };
+
     return (
         <div className={styles.card} style={{ cursor: 'pointer' }}>
             <img src={image} alt={character} />
             <h3>{character}</h3>
             <p>{amiiboSeries}</p>
-            <button onClick={() => addFavorite({ character, image, amiiboSeries, tail })}>Add to Favorites</button>
-            <button onClick={handleDetailsClick}>Detalles</button>
+            <button onClick={handleAddFavorite}>Add to Favorites</button>
+            <button onClick={handleDetailsClick}>Details</button>
         </div>
     );
 };
